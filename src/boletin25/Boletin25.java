a5976e67f377e5c9205eb3ab50c4ce4c91bca02d
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2017-04-21 11:33:09 +0200

    fin

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ejercicio1 uno = new Ejercicio1();
+       //uno.setVisible(true);
+       Ejercicio2 dos = new Ejercicio2();
+       //dos.setVisible(true);
+       Ejercicio3 tres= new Ejercicio3();
+       //tres.setVisible(true);
+       Ejercicio4 cuatro= new Ejercicio4();
+       cuatro.setVisible(true);
+    }
+    
+}
